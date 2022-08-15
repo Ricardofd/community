3900d242484e2c68bdd38977ae252bbb218ac00e
Ricardofd <41286550+Ricardofd@users.noreply.github.com>
2022-08-15 16:26:41 +0800

    redis设置和测试

---
+++ src/main/java/com/newcoder/community/config/RedisConfig.java
@@ -0,0 +1,30 @@
+package com.newcoder.community.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory factory){//声明这个参数bean会自动被容器装配
+        RedisTemplate<String,Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+
+        //设置key的序列化方式
+        template.setKeySerializer(RedisSerializer.string());
+        //设置vlaue的序列化方式
+        template.setValueSerializer(RedisSerializer.json());
+        //设置hash的key的序列化方式
+        template.setHashKeySerializer(RedisSerializer.string());
+        //设置hash的value序列化方式
+        template.setHashValueSerializer(RedisSerializer.json());
+
+        template.afterPropertiesSet();
+        return template;
+    }
+
+}
---
+++ src/test/java/com/newcoder/community/RedisTest.java
@@ -0,0 +1,92 @@
+package com.newcoder.community;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.BoundValueOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class RedisTest {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void testStrings(){
+        String redisKey = "test:count";
+        redisTemplate.opsForValue().set(redisKey,1);
+
+        System.out.println(redisTemplate.opsForValue().get(redisKey));
+        System.out.println(redisTemplate.opsForValue().increment(redisKey));
+        System.out.println(redisTemplate.opsForValue().decrement(redisKey));
+    }
+
+    @Test
+    public void testHashes(){
+        String redisKey = "test:user";
+        redisTemplate.opsForHash().put(redisKey,"id",1);
+        redisTemplate.opsForHash().put(redisKey,"username","zhangsan");
+
+        System.out.println(redisTemplate.opsForHash().get(redisKey,"id"));
+        System.out.println(redisTemplate.opsForHash().get(redisKey,"username"));
+    }
+
+    @Test
+    public void testLists(){
+        String redisKey = "test:ids";
+        redisTemplate.opsForList().leftPush(redisKey,101);
+        redisTemplate.opsForList().leftPush(redisKey,102);
+        redisTemplate.opsForList().leftPush(redisKey,103);
+        System.out.println(redisTemplate.opsForList().size(redisKey));
+        System.out.println(redisTemplate.opsForList().index(redisKey,0));
+        System.out.println(redisTemplate.opsForList().range(redisKey,0,2));
+
+        System.out.println(redisTemplate.opsForList().leftPop(redisKey));
+        System.out.println(redisTemplate.opsForList().rightPop(redisKey));
+    }
+
+    @Test
+    public void testSets(){
+        String redisKey = "test:teachers";
+        redisTemplate.opsForSet().add(redisKey,"刘备","关与","张三","诸葛");
+        System.out.println(redisTemplate.opsForSet().size(redisKey));
+        System.out.println(redisTemplate.opsForSet().pop(redisKey));
+        System.out.println(redisTemplate.opsForSet().members(redisKey));
+    }
+
+    @Test
+    public void testSortedSet(){
+        String redisKey = "test:students";
+        redisTemplate.opsForZSet().add(redisKey,"刘备",98);
+        redisTemplate.opsForZSet().add(redisKey,"刘备2",298);
+        redisTemplate.opsForZSet().add(redisKey,"刘备5",8);
+        redisTemplate.opsForZSet().add(redisKey,"liwww",100);
+        System.out.println(redisTemplate.opsForZSet().zCard(redisKey));
+        System.out.println(redisTemplate.opsForZSet().score(redisKey,"liwww"));
+        System.out.println(redisTemplate.opsForZSet().reverseRange(redisKey,0,2));
+
+    }
+
+    @Test
+    public void testKeys(){
+        redisTemplate.delete("test:user");
+        System.out.println(redisTemplate.hasKey("test:user"));
+    }
+
+    //多次访问同一个key
+    @Test
+    public void testBoundOperations(){
+        String redisKey= "test:count";
+        BoundValueOperations operations = redisTemplate.boundValueOps(redisKey);
+        operations.increment();
+        operations.increment();
+        operations.increment();
+        System.out.println(operations.get());
+    }
+}
