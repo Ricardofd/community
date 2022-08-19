651e853a3e88b9c5c8c760c8f3a50060da83e982
Ricardofd <41286550+Ricardofd@users.noreply.github.com>
2022-08-19 20:03:10 +0800

    关注和粉丝列表

--- src/main/java/com/newcoder/community/controller/UserController.java
+++ src/main/java/com/newcoder/community/controller/UserController.java
@@ -13,7 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +28,7 @@ import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
-@Component
+@Controller
 @RequestMapping("/user")
 public class UserController implements CommunityConstant {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
